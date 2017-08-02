f7a05f4104702099b41020de9040464174b5cc69
cwong2015 <sirhc8002@gmail.com>
2017-08-01 22:55:50 -0400

    implemented basic calculator functionality, need to implement edge cases and foolproof design

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
+// an array of IDs associated with the calculator buttons
 var buttonIDs = ["AC", "CE", "div", "mul", "7", "8", "9", "sub", "4", "5", "6", "add", "1", "2", "3", "mod", "0", "decimal", "equal"];
-var buttonSymbols = ["AC", "CE", "÷", "×", "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "%", "0", ".", "="];
+// an array of symbols associated with the calculator buttons
+var buttonSymbols = ["AC", "CE", "÷", "*", "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "%", "0", ".", "="];
+
+var array = []; // array of operations
 
 $(document).ready(function(){
+    
+    // begin loop to generate html buttons and their IDs/properties onto the calculator UI
   for(var i = 0; i < buttonIDs.length; i++){
       if(i >= 0 && i <= 15){
           var injection;
@@ -27,10 +33,60 @@ $(document).ready(function(){
             $("#injection5").append("<div class='col-md-3'><button id = " + buttonIDs[i] +  " class = 'btn'>" + buttonSymbols[i] + "</button></div>");
           }
       }
-  }
+  } // end of generation
     
+    // eventhandling stuff
     $("button").click(function(){
-        alert(this.id);
-    })
-    
+        if(this.id === "AC"){
+            clearResult();
+            clearArray();
+            $("#display").val("0");
+        }
+        else if(this.id === "CE"){
+            clearResult();
+        }
+        else if(this.id === "equal"){
+            var string = "";
+            for(var i = 0; i < array.length; i++){
+                string+= array[i];
+            }
+            alert(eval(string));
+        }
+        else{
+            var id = this.id;
+            var index = buttonIDs.indexOf(id);
+            var symbol = buttonSymbols[index];
+            array.push(symbol);
+            var val = $("#display").val();
+            $("#display").val(val + symbol);
+            $("#result").val(symbol);
+        }
+    });
 });
+
+//functions useful for clean code/modular design
+function getOperate(token){
+    if(token === "div"){
+        return "/";
+    }
+    else if(token === "mul"){
+        return "*";
+    }
+    else if(token === "sub"){
+        return "-";
+    }
+    else if(token === "add"){
+        return "+";
+    }
+    else if(operator === "mod"){
+        return "%";
+    }
+}
+
+function clearResult(){
+    $("#result").val("0");
+}
+
+function clearArray(){
+    array = [];
+}
